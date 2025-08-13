078473e7c0b7220e1374b100ecc0e23fa51e4033
johnd0e <1838643+johnd0e@users.noreply.github.com>
2025-08-13 23:07:06 +0200

    Update models and docs

--- extra/huggingface-models.sh
+++
@@ -1,9 +0,0 @@
-page=0
-while true; do
-  curl -s "https://huggingface.co/models?other=text-generation-inference&sort=trending&inference=warm&p=$page" |
-    grep 'data-target="ModelList"' |
-    sed -n 's/.*data-props="\([^"]*\)".*/\1/p' |
-    sed 's%&quot;%"%g' |
-    jq -e .initialValues.models[].id || break
-  ((page++))
-done
