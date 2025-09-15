a1033a017865cccbd3879e2d74b3fe62e9def9c8
johnd0e <1838643+johnd0e@users.noreply.github.com>
2025-09-15 21:12:54 +0200

    g4f.*: gen key

---
+++ extra/g4f_gen_key.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+set -e
+UA="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/138.0.0.0 Safari/537.36"
+
+public_key=$(curl -s https://host.g4f.dev/backend-api/v2/public-key)
+public_key_pem=$(mktemp -ut pem.XXXXXX)
+printf '%s' "$public_key" | jq -r .public_key > "$public_key_pem"
+cat <<EOF | openssl pkeyutl -encrypt -inkey "$public_key_pem" -pubin -in - -out - | base64.exe -w 0
+{
+  "data": "$(printf '%s' "$public_key" | jq -r .data)",
+  "user": "$1",
+  "timestamp": $(date +%s%3N),
+  "user_agent": "$UA"
+}
+EOF
+rm "$public_key_pem"
