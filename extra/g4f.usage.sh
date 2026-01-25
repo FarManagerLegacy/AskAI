734775f85436cd2bc060e3dd2792c0d04cc143b7
johnd0e <1838643+johnd0e@users.noreply.github.com>
2026-01-25 12:56:59 +0100

    extra/g4f.select.preset: replace individual presets
    also: g4f.custom*.preset

---
+++ extra/g4f.usage.sh
@@ -0,0 +1,8 @@
+if [ $# -eq 0 ]; then
+    echo "Error: g4f_session local storage value required."
+    echo "Usage: $0 <g4f_session>"
+    exit 1
+fi
+
+curl -s 'https://auth.gpt4free.workers.dev/members/api/usage' \
+  -H "authorization: Bearer $1" | jq
